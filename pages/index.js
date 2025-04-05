4444ebee68b6f5e715a8ee4101cf7a2514f5bbdb
ryanandniaz <DosetarehYadegar@gmail.com>
2025-04-05 03:51:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <div style={{ padding: '56.25% 0 0 0', position: 'relative', height: '100%' }}>
-        <iframe
-          src="https://player.vimeo.com/video/1072709437?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&muted=1&loop=1&background=1"
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-          allowFullScreen
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-          title="Ryan + Niaz Engagement Video"
-        ></iframe>
-      </div>
+    <div style={{
+      position: 'fixed',
+      top: 0, left: 0,
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden',
+      zIndex: -1
+    }}>
+      <iframe
+        src="https://player.vimeo.com/video/1072709437?autoplay=1&muted=1&loop=1&background=1"
+        style={{
+          position: 'absolute',
+          top: 0, left: 0,
+          width: '100%',
+          height: '100%',
+          border: 'none'
+        }}
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      />
     </div>
   );
 }
